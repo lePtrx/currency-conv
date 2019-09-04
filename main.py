928d264d9ded5acc3f43983b8abe9b93f18fcd24
ian lim <ian.lim.tn@gmail.com>
2019-09-04 16:11:38 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# To-do
+# 1. find out finance api for currency converter (fixer.io) - done
+# 2. Allow users to select any of the 3 values (user selection model = in terminal) 
+# 3. build function to convert currency A to currency build
+# 4. Return result
+
+
+
+
+
+# Description of project
+# build a simple terminal currency converter from RM to 3 currencies
+# 1. United States Dollar (USD)
+# 2. Japanese Yen (YEN)
+# 3. Singapore Dollar (SGD)
+
+
+
+import requests
+import json
+
+requests = requests.get('http://data.fixer.io/api/latest?access_key=64462d177fc6f5eddae277f0290382a3&symbols=USD,JPY,SGD')
