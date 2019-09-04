6251f4914fe2c47b22586552eb53e6182dd693f3
lePtrx <leptrx@gmail.com>
2019-09-04 22:10:08 +0800

    Changes from Peter

--- main.py
+++ main.py
@@ -41,22 +41,19 @@ print(SGD)
 print(MYR)
 print(EUR)
 
+<<<<<<< Updated upstream
 # User input of MYR
 amount = float(input("Enter your MYR: "))
+=======
+>>>>>>> Stashed changes
 
-# from MYR to EUR as a base
-msiaRinggit = amount / MYR
-
-# conversion formulas
-conversionUSD = msiaRinggit * USD
-conversionJPY = msiaRinggit * JPY
-conversionSGD = msiaRinggit * SGD
 
 
 # print (conversionUSD)
 # print (conversionJPY)
 # print (conversionSGD)
 
+<<<<<<< Updated upstream
 currencyChoice = int(input("Select your currency:\n1.USD\n2.JPY\n3.SGD\n"))
 
 # while:
@@ -73,3 +70,73 @@ else:
 
 
 
+=======
+#currencyChoice = int(input("Select your currency:\n1.USD\n2.JPY\n3.SGD\n"))
+
+# choice = True
+
+
+# if currencyChoice == 1:
+#     print(conversionUSD)
+# elif currencyChoice == 2:
+#     print(conversionJPY)
+# elif currencyChoice == 3:
+#     print(conversionSGD)
+# else:
+#     print("Please select the right choice.")
+
+def startProgram():
+
+    # User input of MYR
+    print("This program converts MYR to USD, JPY or SGD!")
+
+runloop = 1
+
+while runloop == 1:
+        
+    # Starting the currency conversion program
+    startProgram()
+    amount = int(input("What's your MYR amount?: "))
+
+    # Checking if the amount is of type int or float
+    if type(amount) == int or type(amount) == float:
+
+        # From MYR to EUR as a base
+        msiaRinggit = amount / MYR
+
+        # Conversion formulas
+        conversionUSD = msiaRinggit * USD
+        conversionJPY = msiaRinggit * JPY
+        conversionSGD = msiaRinggit * SGD
+
+        def menu():
+            print("Please select your curreny:")
+            print()
+            print("1.USD")
+            print("2.JPY")
+            print("3.SGD")
+            print("4.Input another amount")
+            print("5.Quit")
+
+        loop = 1
+
+        while loop == 1:
+            menu()
+            currencyChoice = str(input("Your choice? "))
+            if currencyChoice == "1":
+                print(conversionUSD)
+            elif currencyChoice == "2":
+                print(conversionJPY)
+            elif currencyChoice == "3":
+                print(conversionSGD)
+            elif currencyChoice == "4":
+                loop = 0
+                startProgram()        
+            elif currencyChoice == "5":
+                loop = 0
+                print("\nProgramme closed.\n")
+            else:
+                print("\nPlease select from the above option.\n")   
+    else:
+        print("try again")
+>>>>>>> Stashed changes
